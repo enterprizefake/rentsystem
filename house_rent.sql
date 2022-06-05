c2cd26f8562d79f23a5aa8902e4de715c3409c60
pitous-a <1229563042@qq.com>
2022-06-05 18:30:35 +0800

    1

--- house_rent.sql
+++ house_rent.sql
@@ -1,12 +1,12 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2022/6/5 12:07:07                            */
+/* Created on:     2022/6/5 18:28:18                            */
 /*==============================================================*/
 
 
-drop table if exists booking;
+drop table if exists audit;
 
-drop table if exists "check";
+drop table if exists booking;
 
 drop table if exists collection;
 
@@ -16,9 +16,21 @@ drop table if exists house;
 
 drop table if exists message;
 
-drop table if exists "order";
+drop table if exists orders;
 
 drop table if exists uesr;
+
+/*==============================================================*/
+/* Table: audit                                                 */
+/*==============================================================*/
+create table audit
+(
+   audit_id             int not null auto_increment,
+   phone                int,
+   audit_state          varchar(50),
+   audit_info           text,
+   primary key (audit_id)
+);
 
 /*==============================================================*/
 /* Table: booking                                               */
@@ -26,7 +38,7 @@ drop table if exists uesr;
 create table booking
 (
    booking_id           int not null auto_increment,
-   tenants_phone        int,
+   phone                int,
    h_id                 int,
    visit_time           varchar(250),
    visit_number         int,
@@ -36,24 +48,12 @@ create table booking
 );
 
 /*==============================================================*/
-/* Table: "check"                                               */
-/*==============================================================*/
-create table "check"
-(
-   check_id             int not null auto_increment,
-   check_phone          int,
-   check_state          varchar(50),
-   check_info           text,
-   primary key (check_id)
-);
-
-/*==============================================================*/
 /* Table: collection                                            */
 /*==============================================================*/
 create table collection
 (
    collection_id        int not null auto_increment,
-   tenants_phone        int,
+   phone                int,
    h_id                 int,
    primary key (collection_id)
 );
@@ -75,8 +75,8 @@ create table h_picture
 create table house
 (
    h_id                 int not null auto_increment,
-   landlord_phone       int,
-   check_id             int,
+   phone                int,
+   audit_id             int,
    h_name               varchar(300),
    h_state              varchar(20),
    price                int,
@@ -103,12 +103,12 @@ create table message
 );
 
 /*==============================================================*/
-/* Table: "order"                                               */
+/* Table: orders                                                */
 /*==============================================================*/
-create table "order"
+create table orders
 (
    order_id             int not null auto_increment,
-   tenants_phone        int,
+   phone                int,
    h_id                 int,
    begin_date           varchar(500),
    end_date             varchar(500),
@@ -132,16 +132,16 @@ create table uesr
    primary key (phone)
 );
 
-alter table booking add constraint FK_4 foreign key (tenants_phone)
+alter table audit add constraint FK_10 foreign key (phone)
+      references uesr (phone) on delete restrict on update restrict;
+
+alter table booking add constraint FK_4 foreign key (phone)
       references uesr (phone) on delete restrict on update restrict;
 
 alter table booking add constraint FK_7 foreign key (h_id)
       references house (h_id) on delete restrict on update restrict;
 
-alter table "check" add constraint FK_10 foreign key (check_phone)
-      references uesr (phone) on delete restrict on update restrict;
-
-alter table collection add constraint FK_2 foreign key (tenants_phone)
+alter table collection add constraint FK_2 foreign key (phone)
       references uesr (phone) on delete restrict on update restrict;
 
 alter table collection add constraint FK_5 foreign key (h_id)
@@ -150,18 +150,18 @@ alter table collection add constraint FK_5 foreign key (h_id)
 alter table h_picture add constraint FK_8 foreign key (h_id)
       references house (h_id) on delete restrict on update restrict;
 
-alter table house add constraint FK_1 foreign key (landlord_phone)
+alter table house add constraint FK_1 foreign key (phone)
       references uesr (phone) on delete restrict on update restrict;
 
-alter table house add constraint FK_11 foreign key (check_id)
-      references "check" (check_id) on delete restrict on update restrict;
+alter table house add constraint FK_11 foreign key (audit_id)
+      references audit (audit_id) on delete restrict on update restrict;
 
 alter table message add constraint FK_9 foreign key (phone)
       references uesr (phone) on delete restrict on update restrict;
 
-alter table "order" add constraint FK_3 foreign key (tenants_phone)
+alter table orders add constraint FK_3 foreign key (phone)
       references uesr (phone) on delete restrict on update restrict;
 
-alter table "order" add constraint FK_6 foreign key (h_id)
+alter table orders add constraint FK_6 foreign key (h_id)
       references house (h_id) on delete restrict on update restrict;
 
