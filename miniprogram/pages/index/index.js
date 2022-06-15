3452c84b841c45ee4e8180b8350ffb403437984c
ljlhxy <86142958+ljlhxy@users.noreply.github.com>
2022-06-15 08:45:24 +0800

    1

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,116 @@
+// pages/index/index.ts
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    all_houses: null,
+    list:null
+  },
+  to_detail(index){
+    console.log(index.currentTarget.dataset.name)
+    wx.navigateTo({
+      url:"/pages/index/detail/index?house_id="+index.currentTarget.dataset.name
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+
+
+    // wx.getStorageInfoSync
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    wx.showLoading(
+      {
+        title: '加载数据中'
+      }
+    )
+    wx.request({
+      url: 'http://1.15.184.52:8086/index',
+      data: {
+
+      },
+      success: (res) => {
+        var datas = res.data
+       // console.log(datas)
+        if (datas.sucess == 'no') {
+          console.log("???")
+        }
+        else {
+
+          this.setData({
+            all_houses: datas.house
+          })
+          var p=datas.house
+          console.log(p);
+          p.sort(
+            (a,b)=>  (a.price)-(b.price)  
+          );
+          this.setData({
+            list:p.splice(0,3)
+          })
+          
+          console.log(this.data.list);
+          //console.log(this.data.all_houses)
+        }
+        wx.hideLoading()
+      },
+      fail(e) {
+        console.log(e)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
