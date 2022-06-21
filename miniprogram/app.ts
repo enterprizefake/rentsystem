7f5bc3019db2100589af0d7537ec69152006338b
pitous-a <1229563042@qq.com>
2022-06-21 11:21:59 +0800

    1

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,39 +1,25 @@
 // app.ts
 App<IAppOption>({
   globalData: {
-    user:null
+    user: null
   },
-  login()
-  {
-
+  login() {
     wx.navigateTo({
-      url:"/pages/login/index"
+      url: "/pages/login/index"
     })
-      // wx.getUserProfile({
-      //   desc: '用于登录', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      //   success: (res) => {
-      //     this.globalData.user=res.userInfo           
-      //     wx.setStorageSync("user",res.userInfo)
-      //   }
-      // })
-
-      wx.getUserProfile({
-        desc: '用于登录', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-        success: (res) => {
-
-          
-            this.globalData.user=res.userInfo        
-
-          wx.setStorageSync("user",res.userInfo)
-        }
-      })
+    // wx.getUserProfile({
+    //   desc: '用于登录', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+    //   success: (res) => {
+    //     this.globalData.user=res.userInfo           
+    //     wx.setStorageSync("user",res.userInfo)
+    //   }
+    // })
 
   },
   onLaunch() {
     const user = wx.getStorageSync('user')
-    if(user)
-    {
-      this.globalData.user=user
+    if (user) {
+      this.globalData.user = user
     }
 
     // // 登录
