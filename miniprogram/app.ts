f9e99480a4742dba519e9c5f8507c30c9a58f997
pitous-a <1229563042@qq.com>
2022-06-21 22:23:40 +0800

    1

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -4,29 +4,27 @@ App<IAppOption>({
     user: null
   },
   login() {
-      var temp_user=null
-      wx.getUserProfile({
+    var temp_user = null
+    wx.getUserProfile({
       desc: '用于登录', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        temp_user=res.userInfo
+        temp_user = res.userInfo
         wx.getLocation({
           type: 'wgs84',
-          success: (res)=> {
-            temp_user={...temp_user,...res}
-            
+          success: (res) => {
 
+            temp_user = { ...temp_user, ...res }
             wx.navigateTo({
-              url:"/pages/login/index",
-              success: function(res)=>{
-                this.globalData.user=temp_user
+              url: "/pages/login/index",
+              success: (res) => {
+                this.globalData.user = temp_user
               }
             })
 
           }
-         })
+        })
       }
     })
-
   },
   onLaunch() {
     const user = wx.getStorageSync('user')
