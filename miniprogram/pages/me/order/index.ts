4a6e9d97343e17396142ab0dd179ef04dd2cbb6d
pitous-a <1229563042@qq.com>
2022-06-22 19:39:36 +0800

    1

--- miniprogram/pages/me/order/index.ts
+++ miniprogram/pages/me/order/index.ts
@@ -1,13 +1,39 @@
-// pages/me/order/index.ts
+import { formatTime } from '../../../utils/util'
+var app=getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    orders:null,
+    date:null
   },
-  onLoad: function (option) {
-    console.log(option)
-  }
+  onLoad (data) {
+    this.setData({
+      date:formatTime(new Date())
+    })
+    if( data.user_type == "tenant" )
+    {
+      data.user_type="tenants"
+      wx.request({
+        url:"http://127.0.0.1:8086/"+data.user_type+"/orders",
+        method:'POST',
+        data:{
+          phone:app.globalData.user.phone
+        },
+        success:(res)=>
+        {
+          this.setData({
+            orders:res.data.orders
+          })
+
+        }
+
+        
+      })
+    }
+
+
+}
 })
