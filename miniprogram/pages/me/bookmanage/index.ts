cf66044ab2bb134bf6fde1bfb5a7feab2827317c
ljlhxy <86142958+ljlhxy@users.noreply.github.com>
2022-06-22 19:43:32 +0800

    1

--- miniprogram/pages/me/bookmanage/index.ts
+++ miniprogram/pages/me/bookmanage/index.ts
@@ -5,16 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+  all_booking:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-
+    var app = getApp()
+    wx.showLoading(
+      {
+        title: '加载数据中'
+      }
+    )
+    wx.request({
+      // url: 'http://1.15.184.52:8086/index',
+      url: 'http://127.0.0.1:8086/landlord/book',
+      method:"POST",
+      data: {
+        phone:2
+      },
+      success: (res) => {
+        var datas = res.data
+        console.log(datas)
+        if (datas.sucess == 'no') {
+          console.log("???")
+        }
+        else {
+          this.setData({
+            all_booking:datas.bookings
+          })
+          //console.log(this.data.all_booking)
+        }
+        wx.hideLoading()
+      },
+      fail(e) {
+        console.log(e)
+      }
+    })
   },
+ book_detaile(){
 
+ },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
