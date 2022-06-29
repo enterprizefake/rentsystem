83a1edb6ab9053b146897689cbd27032c17b98eb
pitous-a <1229563042@qq.com>
2022-06-29 13:30:34 +0800

    1

--- miniprogram/pages/me/order/index.ts
+++ miniprogram/pages/me/order/index.ts
@@ -5,19 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
+    user_type:null,
     orders: null,
     show: null,
     card_margin: 0
   },
   onLoad(data) {
     if (data.user_type == "tenant") {
-      data.user_type = "tenants"
+      this.setData({
+        user_type:"tenants"
+      })
+    }
+    else
+    {
+      this.setData({
+        user_type:"landlord"
+      })
     }
     wx.showLoading({
       title: '加载中'
     });
     wx.request({
-      url: "http://127.0.0.1:8086/" + data.user_type + "/orders",
+      url: "http://127.0.0.1:8086/" + this.data.user_type + "/orders",
       method: 'POST',
       data: {
         phone: app.globalData.user.phone
@@ -34,6 +43,9 @@ Page({
 
   },
   to_detail(v) {
+    if (this.data.user_type == "landlord") {
+      v.currentTarget.dataset.name.phone=app.globalData.user.phone
+    }
     wx.navigateTo({
       url: "/pages/index/detail/index?house=" + encodeURIComponent(JSON.stringify(v.currentTarget.dataset.name))
     })
