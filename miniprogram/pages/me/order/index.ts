46ee1b342bb36ee6f800734684e332784cf9994e
pitous-a <1229563042@qq.com>
2022-06-29 12:06:33 +0800

    1

--- miniprogram/pages/me/order/index.ts
+++ miniprogram/pages/me/order/index.ts
@@ -12,22 +12,20 @@ Page({
   onLoad(data) {
     if (data.user_type == "tenant") {
       data.user_type = "tenants"
-      wx.request({
-        url: "http://127.0.0.1:8086/" + data.user_type + "/orders",
-        method: 'POST',
-        data: {
-          phone: app.globalData.user.phone
-        },
-        success: (res) => {
-          this.setData({
-            orders: res.data.orders
-          })
-
-        }
-
-
-      })
     }
+    wx.request({
+      url: "http://127.0.0.1:8086/" + data.user_type + "/orders",
+      method: 'POST',
+      data: {
+        phone: app.globalData.user.phone
+      },
+      success: (res) => {
+        this.setData({
+          orders: res.data.orders
+        })
+      }
+    })
+
 
 
   },
