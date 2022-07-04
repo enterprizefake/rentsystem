d9da875243edf5719102b4abbf005d85afe89891
pitous-a <1229563042@qq.com>
2022-07-04 20:23:04 +0800

    1

--- miniprogram/pages/me/root/index.ts
+++ miniprogram/pages/me/root/index.ts
@@ -8,11 +8,13 @@ Page({
     audited: null,
     card: 'card',
     user_list: null,
-    developers:null,
+    developers: null,
     roots: null,
     me: null,
     now_index: null,
-    show_list:false
+    show_list: false,
+    top: null,
+    top_index: 'me'
   },
   check(index) {
     wx.navigateTo({
@@ -26,6 +28,14 @@ Page({
 
   },
   onShow() {
+    var query = wx.createSelectorQuery()
+    query.select('#top').boundingClientRect()
+    query.selectViewport().scrollOffset()
+    query.exec((res) => {
+      this.setData({
+        top: res[0].top
+      })
+    });
     wx.request({
       url: "http://1.15.184.52:8086/root/audits",
       method: "POST",
@@ -34,7 +44,7 @@ Page({
         this.setData({
           audits: res.data.audits
         })
-        console.log(res.data.audits)
+        // console.log(res.data.audits)
       }
     });
 
@@ -49,7 +59,7 @@ Page({
         this.setData({
           audited: res.data.audited
         })
-        console.log("audited:" + res.data)
+        // console.log("audited:" + res.data)
       }
 
     });
@@ -62,7 +72,7 @@ Page({
         phone: app.globalData.user.phone
       },
       success: (res) => {
-        console.log(res.data)
+        // console.log(res.data)
         var list = res.data.user_list;
         var temp = list['#'];
         delete list['#']
@@ -71,7 +81,7 @@ Page({
           user_list: list,
           me: res.data.me,
           roots: res.data.roots,
-          developers:res.data.developers
+          developers: res.data.developers
         })
       }
 
@@ -92,20 +102,19 @@ Page({
     temp_roots.push(root);
     ls[index1].splice(index2, 1);
     this.setData({
-      user_list:ls
+      user_list: ls
     });
     wx.request({
-      url:"http://1.15.184.52:8086/root/alterroots",
-      method:'POST',
+      url: "http://1.15.184.52:8086/root/alterroots",
+      method: 'POST',
       header: { "content-type": "application/json" },
       data:
       {
-        roots:temp_roots,
-        myphone:this.data.me.phone,
-        developers:this.data.developers
+        roots: temp_roots,
+        myphone: this.data.me.phone,
+        developers: this.data.developers
       },
-      success:(res)=>
-      {
+      success: (res) => {
         console.log(res.data)
         this.onShow()
       }
@@ -120,35 +129,69 @@ Page({
       roots: ls
     })
     wx.request({
-      url:"http://1.15.184.52:8086/root/alterroots",
-      method:'POST',
+      url: "http://1.15.184.52:8086/root/alterroots",
+      method: 'POST',
       header: { "content-type": "application/json" },
       data:
       {
-        roots:this.data.roots,
-        myphone:this.data.me.phone,
-        developers:this.data.developers
+        roots: this.data.roots,
+        myphone: this.data.me.phone,
+        developers: this.data.developers
       },
-      success:(res)=>
-      {
+      success: (res) => {
         console.log(res.data)
         this.onShow();
       }
     });
   },
-  show_list(v)
-  {
-    if(v.detail.index==2)
-    {
+  show_list(v) {
+    if (v.detail.index == 2) {
       this.setData({
-        show_list:true
+        show_list: true
       })
     }
-    else
-    {
+    else {
       this.setData({
-        show_list:false
+        show_list: false
       })
     }
+  },
+  inid(id) {
+    var query = wx.createSelectorQuery()
+    query.select('#' + id).boundingClientRect()
+    query.selectViewport().scrollOffset()
+    return new Promise((resolve) => {
+      query.exec((res) => {
+          if ((res[0].top - this.data.top <= 0) && (res[0].bottom - this.data.top >= 0)) {
+            resolve(true);
+          }
+          else {
+            resolve(false);
+          }
+      });
+    })
+  },
+  async scroll(v) {
+    if (await this.inid('me')) {
+      this.setData({
+        top_index: 'me'
+      })
+    }
+    else if (await this.inid('roots')) {
+      this.setData({
+        top_index: 'roots'
+      })
+    }
+    else {
+      for (var i in this.data.user_list) {
+        if(i=='#') i='all';
+        if (await this.inid(i)) {
+          this.setData({
+            top_index: i
+          });
+        };
+      };
+    };
+
   }
 })
